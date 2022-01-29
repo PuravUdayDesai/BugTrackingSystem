9c7e0ef5b334cfa3b21470dcd7307ec5586dd2ad
PuravUdayDesai <purav7838@gmail.com>
2022-01-29 17:02:34 +0530

    Initial commit

---
+++ src/main/java/trinity/bug/bounty/BugBountyApplication.java
@@ -0,0 +1,13 @@
+package trinity.bug.bounty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BugBountyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BugBountyApplication.class, args);
+	}
+
+}
